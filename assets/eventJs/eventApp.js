26effcf2fd8a4f384c916eea6ae0ecc6086a021c
Stark <cinq.spartacus@gmail.com>
2022-04-04 17:53:10 +0200

    contact template formulaire

--- assets/eventJs/eventApp.js
+++ assets/eventJs/eventApp.js
@@ -1,1 +1,11 @@
-alert('Bonjour');
+
+const autre = document.querySelector('.autre-message');
+const addClass = document.querySelector('div .display-message ');
+
+
+
+autre.onclick = ()=>{
+
+    addClass.classList.add("apparai-message");
+
+}
