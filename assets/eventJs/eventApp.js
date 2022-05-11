7bd9ed7d337c4c745646201c3606074bdd417646
Stark <cinq.spartacus@gmail.com>
2022-05-11 18:15:14 +0200

    contact professionel
    contact professionel mise en place

--- assets/eventJs/eventApp.js
+++ assets/eventJs/eventApp.js
@@ -2,15 +2,22 @@
 const particulier = document.querySelector('.autre-message');
 
 
-
-const addClass = document.querySelector('div .display-message ');
-
-
+const addClassParticulier = document.querySelector('div .display-message ');
 
 particulier.onclick = ()=>{
 
-    addClass.classList.add("apparai-message");
+    addClassParticulier.classList.add("apparai-message");
 
 
+}
+
+
+
+const professionel = document.querySelector('.professionel-message');
+
+const addClassProfessionel = document.querySelector('div .display-message-pro ');
+
+professionel.onclick =() =>{
+    addClassProfessionel.classList.add("apparai-block-message-pro");
 }
 
